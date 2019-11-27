80595220aa4cc65650b00c900a2315388383593b
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2019-11-27 21:38:30 +0530

    Create array_manipulation.js

---
+++ hard/array_manipulation.js
@@ -0,0 +1,17 @@
+/*
+https://www.hackerrank.com/challenges/crush/problem
+*/
+function arrayManipulation(n, queries) {
+    var max = 0, sum=0, result = new Array(n + 1).fill(0);
+    
+    for(let i = 0; i < queries.length; i++){
+        const [start,end,value] = queries[i];
+        result[start - 1] += value;
+        result[end] -= value;
+    }
+    for(let j = 0; j < n; j++){
+        sum += result[j];
+        max = Math.max(max,sum);
+    }
+    return max;
+}
