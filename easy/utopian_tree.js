36eb5968b7194288d1a2c2c4e82e0a7a63f53ba1
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2019-11-30 00:27:12 +0530

    Create utopian_tree.js

---
+++ easy/utopian_tree.js
@@ -0,0 +1,15 @@
+/*
+https://www.hackerrank.com/challenges/utopian-tree/problem
+*/
+function utopianTree(n) {
+    var height = 1;
+    for(var i = 1; i <= n; i++){
+        if(i % 2 == 0){
+            height += 1;
+        }
+        else{
+            height *= 2;
+        }
+    }
+    return height;
+}
