eb40da6c9ddcbfdd7277a5cd48a3046a17fa75ac
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2019-12-07 11:50:20 +0530

    Create arithmetic_operators.js

---
+++ 10_days_of_js/arithmetic_operators.js
@@ -0,0 +1,12 @@
+/*
+https://www.hackerrank.com/challenges/js10-arithmetic-operators/problem
+*/
+function getArea(length, width) {
+    let area = length * width;
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter = 2 * (length + width);
+    return perimeter;
+}
