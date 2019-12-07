98fa1157188ed7bfa0e9c0e38c3d11e6e7c64977
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2019-12-07 11:52:51 +0530

    Create let_and_const.js

---
+++ 10_days_of_js/let_and_const.js
@@ -0,0 +1,9 @@
+/*
+https://www.hackerrank.com/challenges/js10-let-and-const/problem
+*/
+function main() {
+    var r = readLine();
+    const PI = Math.PI;
+    console.log(PI * r * r);
+    console.log(2 * PI * r);
+}
