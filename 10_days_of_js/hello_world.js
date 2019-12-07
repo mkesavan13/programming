534d5f228fc3d1fccab6a1a530d33f0b53fb8e82
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2019-12-07 11:46:22 +0530

    Create hello_world.js

---
+++ 10_days_of_js/hello_world.js
@@ -0,0 +1,10 @@
+/*
+https://www.hackerrank.com/challenges/js10-hello-world/problem
+*/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    
+}
