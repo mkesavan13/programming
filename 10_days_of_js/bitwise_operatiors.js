c1984e0cf480356d6207706928e8886eaf4e2d50
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2019-12-08 18:15:03 +0530

    Create bitwise_operatiors.js

---
+++ 10_days_of_js/bitwise_operatiors.js
@@ -0,0 +1,14 @@
+/*
+https://www.hackerrank.com/challenges/js10-bitwise/problem
+*/
+function getMaxLessThanK(n, k) {
+    let max_anb = 0;
+    for (let b = n; b > 0; b--) {
+        for (let a = b-1; a > 0; a--) {
+            if ((a & b) < k && (a & b) > max_anb){
+                max_anb = (a&b);
+            }
+        }
+    }
+    return max_anb;
+}
