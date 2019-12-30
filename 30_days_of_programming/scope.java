bb9730d7b2be507899ee4e1c5a85542a61ce7a10
Kesava Krishnan Madavan <mkesavan13@gmail.com>
2019-12-30 08:15:02 +0530

    Create scope.java

---
+++ 30_days_of_programming/scope.java
@@ -0,0 +1,45 @@
+/*
+https://www.hackerrank.com/challenges/30-scope/problem
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+class Difference {
+  	private int[] elements;
+  	public int maximumDifference;
+    public Difference(int[] elems){
+        this.elements = elems;
+    }
+
+    public void computeDifference(){
+        int[] elements = this.elements;
+        java.util.Arrays.sort(elements);
+        this.maximumDifference = Math.abs(elements[elements.length - 1] - elements[0]);
+    }
+	// Add your code here
+
+} // End of Difference class
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
